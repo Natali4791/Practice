67ea999fe30b3633e2c65d3607e3ddfe87dc1d4e
Natali4791 <pelinat74@gmail.com>
2025-01-05 19:25:05 +0300

    position gradients display float

---
+++ 004-chapter/020-interactive-css-display/scripts.js
@@ -0,0 +1,10 @@
+const changeDisplay = (displayValue) => {
+  [document.getElementById("rectangle1"), document.getElementById("rectangle2")].forEach(
+    (e) => (e.style.display = displayValue)
+  );
+};
+const changeWidth = (width) => {
+  [document.getElementById("rectangle1"), document.getElementById("rectangle2")].forEach(
+    (e) => (e.style.width = width)
+  );
+};
