cd857aff9d7670b88507e5ffab2a387d57740578
Natali4791 <pelinat74@gmail.com>
2025-04-11 17:25:11 +0300

    обработка исключение try catch

---
+++ JAVASCRIPT-MASTERY-JOURNEY/029-exception-handling/ex-00.js
@@ -0,0 +1,12 @@
+const obj = {
+  foo: "foo value",
+  bar: "bar value",
+};
+
+try {
+  console.log(obj.foo());
+} catch {
+  console.log("что-то пошло не так");
+} finally {
+  console.log("finally");
+}
