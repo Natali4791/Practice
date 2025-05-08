9a1ab7351bf05d5b22368d76aa0af5fb03c3a43c
Natali4791 <pelinat74@gmail.com>
2025-05-08 12:29:58 +0300

    файл hooks/mycomponents.jsx

--- react-quick-start/src/index.js
+++ react-quick-start/src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./css/index.css";
 import HomePage from "./Layout/HomePage/HomePage";
+import MyComponents from "./Layout/Hooks/MyComponents";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HomePage />);
+root.render(
+  //<HomePage />
+  <MyComponents />
+);
