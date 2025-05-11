26b42a748af2f5d3f1d28ad1d9f689e744cc9867
Natali4791 <pelinat74@gmail.com>
2025-05-11 15:20:33 +0300

    react приложение, хуки, api, axios

--- react-quick-start/src/Layout/HomePage/HomePage.jsx
+++ react-quick-start/src/Layout/HomePage/HomePage.jsx
@@ -4,6 +4,8 @@ import ContentItem from "./components/ContentItem";
 import HeaderItem from "./components/HeaderItem";
 import FooterItem from "./components/FooterItem";
 const baseUrl = "https://jsonplaceholder.typicode.com/posts";
+// const imgUrl = "https://jsonplaceholder.typicode.com/photos";
+
 const HomePage = () => {
   const title = "Новое название карточек";
   const [labels, setLabels] = useState([
@@ -14,26 +16,29 @@ const HomePage = () => {
     // { id: 5, cardTitle: "Заголовок 5", cardText: "Описание 5" },
     // { id: 6, cardTitle: "Заголовок 6", cardText: "Описание 6" },
   ]);
+  // const [pictures, setPictures] = useState([]);
+  // useEffect(() => {
+  //   axios.get(imgUrl).then((res1) => {console.log(res1);
 
   useEffect(() => {
     axios.get(baseUrl).then((res) => {
-      const temp = [];
-      res.data.slice(0, 5).forEach(
-        // {
-        // userId,
-        // id,
-        // title,
-        // body
-        // }
-        (item) => {
-          temp.push({
-            id: item.id,
-            cardTitle: item.title.slice(0, 10),
-            cardText: item.body.slice(0, 50),
-          });
-        }
-      );
-      setLabels(temp);
+      // const temp = [];
+      setLabels(res.data.slice(0, 5)); /*.forEach(*/
+      // {
+      // userId,
+      // id,
+      // title,
+      // body
+      // }
+      // (item) => {
+      //   temp.push({
+      //     id: item.id,
+      //     cardTitle: item.title.slice(0, 10),
+      //     cardText: item.body.slice(0, 50),
+      //   });
+      // }
+      /*);*/
+      // setLabels(temp);
     });
   });
   return (
@@ -41,7 +46,17 @@ const HomePage = () => {
       <HeaderItem headerText={title} />
       <ContentItem setOfLabels={labels} />
 
-      {/* <button
+      <FooterItem />
+    </div>
+  );
+};
+
+export default HomePage;
+
+//{ id: 1, cardTitle: "Новый Заголовок 1", cardText: "Новое Описание 1" }
+
+{
+  /* <button
         className="btn btn-primary m-3"
         onClick={() => {
           setLabels([
@@ -51,12 +66,5 @@ const HomePage = () => {
         }}
       >
         Добавить элемент
-      </button> */}
-
-      <FooterItem />
-    </div>
-  );
-};
-export default HomePage;
-
-//{ id: 1, cardTitle: "Новый Заголовок 1", cardText: "Новое Описание 1" }
+      </button> */
+}
--- react-quick-start/src/Layout/HomePage/components/ContentItem.jsx
+++ react-quick-start/src/Layout/HomePage/components/ContentItem.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
 const ContentItem = (props) => {
   return (
     <div>
       {props.setOfLabels.map((item, index) => (
-        <div className="card m-3">
+        <div key={item.id} className="card m-3">
           <img
             src={`https://jsonplaceholder.typicode.com/photos`}
             className="card-img-top"
-            alt="Мои картинки"
+            /*{item.cardImg}*/ alt="Фейковые картинки"
           />
           <div className="card-body">
-            <h5 className="card-title">{item.cardTitle}</h5>
-            <p className="card-text">{item.cardText}</p>
+            <h5 className="card-title">{item.title.slice(0, 30)}</h5>
+            <p className="card-text">{item.body.slice(0, 50)}</p>
           </div>
         </div>
       ))}
